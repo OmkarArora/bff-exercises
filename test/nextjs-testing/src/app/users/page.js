3d6c481ae791d805733fc1f7182dc3a63029e895
Omkar Arora <omkar@sparklin.com>
2023-08-20 12:13:29 +0530

    fix: added try catch blocks for api request

--- test/nextjs-testing/src/app/users/page.js
+++ test/nextjs-testing/src/app/users/page.js
@@ -1,6 +1,12 @@
 export default async function Page() {
-	const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/users`);
-	const data = await res.json();
-	console.log("API data", data);
-	return <div>PAGe loaded !</div>;
+	let dataToShow = "";
+	try {
+		const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/users`);
+		const data = await res.json();
+		dataToShow = JSON.stringify(data);
+	} catch (error) {
+		dataToShow = JSON.stringify(error);
+	}
+
+	return <div>PAGe loaded ! Api response - {dataToShow}</div>;
 }
