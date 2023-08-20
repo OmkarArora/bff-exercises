5d309c90813c3d5a87b410efaa29688b24f31a56
Omkar Arora <omkar@sparklin.com>
2023-08-20 11:00:36 +0530

    changed loading timing

--- test/nextjs-testing/src/app/users/page.js
+++ test/nextjs-testing/src/app/users/page.js
@@ -1,12 +1,10 @@
-export const revalidate = 0;
-
 function later(delay) {
 	return new Promise(function (resolve) {
 		setTimeout(resolve, delay);
 	});
 }
 
-export default async function Page(){
-    await later(10000)
-    return <div>PAGe loaded !</div>
+export default async function Page() {
+	await later(5000);
+	return <div>PAGe loaded !</div>;
 }
