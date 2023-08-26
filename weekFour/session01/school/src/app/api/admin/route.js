63936bc5283f7dc54ae456a73388dcc481baeb61
Omkar Arora <omkar@sparklin.com>
2023-08-26 12:17:25 +0530

    added try-catch

--- weekFour/session01/school/src/app/api/admin/route.js
+++ weekFour/session01/school/src/app/api/admin/route.js
@@ -6,47 +6,55 @@ import { sign } from "@/utils/jwt";
 
 export async function POST(req) {
 	const { email, password } = await req.json();
-
-	const requestedAdmin = await prisma.admin.findFirst({
-		where: {
-			email: {
-				equals: email,
-			},
-		},
-	});
-
-	if (requestedAdmin) {
-		const isPasswordValid = await compare(requestedAdmin.password, password);
-
-		// to avoid truthy, falsy JS values
-		if (isPasswordValid === true) {
-			const token = sign({
-				accessLevel: "admin",
-				email: requestedAdmin.email,
-			});
-			const cookieStore = cookies();
-			const oneHour = 60 * 60 * 1000;
-			cookieStore.set({
-				name: "token",
-				value: token,
-				httpOnly: true,
-				path: "/",
-				expires: Date.now() + oneHour,
-			});
-
-			return NextResponse.json(
-				{
-					message: "User logged in",
+	try {
+		const requestedAdmin = await prisma.admin.findFirst({
+			where: {
+				email: {
+					equals: email,
 				},
-				{ status: 200 }
-			);
-		}
-	}
+			},
+		});
 
-	return NextResponse.json(
-		{
-			error: "invalid credentials",
-		},
-		{ status: 400 }
-	);
+		if (requestedAdmin) {
+			const isPasswordValid = await compare(requestedAdmin.password, password);
+
+			// to avoid truthy, falsy JS values
+			if (isPasswordValid === true) {
+				const token = sign({
+					accessLevel: "admin",
+					email: requestedAdmin.email,
+				});
+				const cookieStore = cookies();
+				const oneHour = 60 * 60 * 1000;
+				cookieStore.set({
+					name: "token",
+					value: token,
+					httpOnly: true,
+					path: "/",
+					expires: Date.now() + oneHour,
+				});
+
+				return NextResponse.json(
+					{
+						message: "User logged in",
+					},
+					{ status: 200 }
+				);
+			}
+		}
+
+		return NextResponse.json(
+			{
+				error: "invalid credentials",
+			},
+			{ status: 400 }
+		);
+	} catch (error) {
+		return NextResponse.json(
+			{
+				error: "something went wrong",
+			},
+			{ status: 500 }
+		);
+	}
 }
